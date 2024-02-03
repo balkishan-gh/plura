50c3f854344f0ecc0e9fd8488b3538fa16f570bc
Bal Kishan <bkunsolved@gmail.com>
2024-02-03 16:48:10 +0530

    create landing page

---
+++ src/app/site/layout.tsx
@@ -0,0 +1,12 @@
+import Navigation from "@/components/site/navigation";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="h-full">
+      <Navigation />
+      {children}
+    </main>
+  );
+};
+
+export default Layout;
